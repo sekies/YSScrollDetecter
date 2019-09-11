26ae9672ef88b2ebb981caa003bf293153131b7d
Yosuke <y.seki@bravesoft.co.jp>
2019-09-11 12:18:42 +0900

    Initial Commit

---
+++ YSScrollDetecter/YSScrollDetecter.h
@@ -0,0 +1,19 @@
+//
+//  YSScrollDetecter.h
+//  YSScrollDetecter
+//
+//  Created by 関　洋輔 on 2019/09/11.
+//  Copyright © 2019 関　洋輔. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for YSScrollDetecter.
+FOUNDATION_EXPORT double YSScrollDetecterVersionNumber;
+
+//! Project version string for YSScrollDetecter.
+FOUNDATION_EXPORT const unsigned char YSScrollDetecterVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <YSScrollDetecter/PublicHeader.h>
+
+
